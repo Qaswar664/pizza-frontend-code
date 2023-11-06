714f73e4c9aeeb2a1fb9ffddb9661d856388425b
Qaswar664 <qaswarwebtechahmad@gmail.com>
2023-11-06 10:58:16 +0500

    details side bar on card click added

--- src/components/Pizza.jsx
+++ src/components/Pizza.jsx
@@ -1,14 +1,19 @@
 import React, { useState } from "react";
 import { Button, Card, Col, Row } from "react-bootstrap";
+import Offcanvas from 'react-bootstrap/Offcanvas';
+
 
 const Pizza = ({ pizza }) => {
   const [variant, setVariant] = useState("small");
   const [quantity, setQuantity] = useState(1);
-  console.log(variant, "variant");
-  console.log(quantity, "quantity");
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
+    <>
     <Card style={{ width: "18rem" }} className="shadow m-5 p-1">
-      <Card.Img variant="top" src={pizza.image} />
+      <Card.Img variant="top" src={pizza.image} onClick={handleShow} style={{cursor:'pointer'}}/>
       <Card.Body>
         <Card.Title>{pizza.name}</Card.Title>
         <Card.Text>
@@ -54,6 +59,23 @@ const Pizza = ({ pizza }) => {
         </Row>
       </Card.Body>
     </Card>
+
+    {/* // details sidbar  */}
+    
+    <Offcanvas  show={show} onHide={handleClose} placement="end">
+    <Offcanvas.Header closeButton>
+      <Offcanvas.Title>{pizza.name}</Offcanvas.Title>
+    </Offcanvas.Header>
+    <Offcanvas.Body>
+      <div className="border p-3">
+      <Card.Img variant="top" src={pizza.image}/>
+      </div>
+      <div className="border p-3">
+        {pizza.description}
+      </div>
+    </Offcanvas.Body>
+  </Offcanvas>
+  </>
   );
 };
 
